0d1f74ade321ff5f1b24b1bf9c782584e1225e4b
wbculver <wbculver@gmail.com>
2023-05-26 16:55:04 -0400

    modified:   aws-transit.tf 	modified:   terraform.tfstate 	modified:   terraform.tfstate.backup

--- aws-transit.tf
+++ aws-transit.tf
@@ -10,7 +10,7 @@
 ##############################################################################################################################
 
 # create tgw in AWS Network Account 
-resource "aws_ec2_transit_gateway" "test-tgw" {
+resource "aws_ec2_transit_gateway" "tgw" {
  
   description                     = "Transit Gateway testing scenario with 2 VPCs, subnets each"
   default_route_table_association = "enable"
@@ -30,27 +30,28 @@ resource "aws_vpc" "aws_transit_vpc" {
 output "aws_transit_vpc_out" {
   value = aws_vpc.aws_transit_vpc.id
 }
+resource "aws_subnet" "tgw_subnet" {
+  vpc_id     = aws_vpc.aws_transit_vpc.id  
+  cidr_block = "10.192.1.0/24"       # Replace with your desired CIDR block for the subnet
+  availability_zone = "us-east-1a" # Replace with your desired availability zone
+
+  tags = {
+    Name = "tgwSubnet"              # Replace with your desired name for the subnet
+  }
+}
+
 ## Attachement of VPC1 from AWS production Account
 
-#resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1-attachment" {
-# 
-#  subnet_ids         = [aws_subnet.public_subnet1.id]
-#  transit_gateway_id = aws_ec2_transit_gateway.test-tgw.id
-#  vpc_id             = aws_vpc.vpc1.id
-#  tags = {
-#    "Name" = "transit-gateway-attachment1"
-#  }
-#}
-#
-### Attachement of VPC2 from AWS production Account
-#resource "aws_ec2_transit_gateway_vpc_attachment" "vpc2-attachment" {
-# 
-#  subnet_ids         = [aws_subnet.public_subnet2.id]
-#  transit_gateway_id = aws_ec2_transit_gateway.test-tgw.id
-#  vpc_id             = aws_vpc.vpc2.id
-#  tags = {
-#    "Name" = "transit-gateway-attachment2"
-#  }
-#}
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1-attachment" {
+ 
+  subnet_ids         = [aws_subnet.tgw_subnet.id]
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  vpc_id             = aws_vpc.aws_transit_vpc.id 
+  tags = {
+    "Name" = "transit-gateway-attachment1"
+  }
+}
 
-
+output "aws_transitgw_out" {
+  value = aws_ec2_transit_gateway.tgw.id
+}
