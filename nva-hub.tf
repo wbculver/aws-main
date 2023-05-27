45561c73f2d6b4018671d960e86426f3f5d2337a
wbculver <wbculver@gmail.com>
2023-05-27 01:37:07 -0400

    modified:   nva-hub.tf

--- nva-hub.tf
+++ nva-hub.tf
@@ -27,8 +27,32 @@ resource "aws_route_table" "public_subnet1" {
 
 } 
 
-# Create route table public subnet association
-#resource "aws_route_table_association" "public_subnet_association1" {
-#  subnet_id      = aws_subnet.public_subnet1.id
-#  route_table_id = aws_route_table.public_subnet1.id
-#}#
+#esource "aws_panos_firewall" "main" {
+#   name = "main-fw"
+#   region = "us-east-1a"
+#   ami_id = "ami.6njl1pau431dv1qxipg63mvah"
+#   vpc_id = aws_vpc.main.id
+#   instance_type = "m4.xlarge"
+#   key_name = "pa-key"
+#
+#   network_interfaces = [
+#     {
+#       device_index = 0
+#       subnet_id = aws_vpc.main.subnets[0].id
+#     },
+#     {
+#       device_index = 1
+#       subnet_id = aws_vpc.main.subnets[1].id
+#     }
+#   ]
+#
+#   route_tables = [
+#     {
+#       id = aws_vpc.main.route_tables[0].id
+#     }
+#   ]
+#
+#   tags = {
+#     "Name" = "Main Palo Alto Firewall"
+#   }
+# }
