e0d2afd939f4edb73b6440ee2d487ce8479766ce
wbculver <wbculver@gmail.com>
2023-05-27 00:52:49 -0400

    new file:   .terraform.tfstate.lock.info 	modified:   aws-main.tf 	deleted:    backend.tfvars 	deleted:    out.yml 	new file:   spoke1-network.tf

--- aws-main.tf
+++ aws-main.tf
@@ -1,38 +1,8 @@
 
-
 provider "aws" {
   shared_credentials_files = ["~/.aws/credentials"]
   region = "us-east-1"
 }
-
-resource "aws_s3_bucket" "a2023-tf-bc-my-terraform-state-buckett" {
-  bucket = "a2023-tf-bc-my-terraform-state-buckett"
-
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_dynamodb_table" "my_terraform_state_lock_table" {
-  name = "my_terraform_state_lock_table"
-  hash_key = "id"
-  range_key = "version"
-  read_capacity = 1
-  write_capacity = 1
-
-  billing_mode = "PROVISIONED"
-
-  attribute {
-    name = "id"
-    type = "N"
-  }
-
-  attribute {
-    name = "version"
-    type = "N"
-  }
-}
-
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
--- backend.tfvars
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "a2023-tf-bc-my-terraform-state-buckett"
-    region         = "us-east-1"
-    key            = "terraform.tfstate"
-    dynamodb_table = "my-terraform-state-lock-table"
-  }
-}
