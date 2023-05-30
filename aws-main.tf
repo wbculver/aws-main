9f48b3e2e0d3c82f9ecc42acd7f7b9c98bfb5fca
wbculver <wbculver@gmail.com>
2023-05-30 10:49:21 -0400

    modified:   aws-main.tf 	modified:   aws-transit.tf 	modified:   nva-hub.tf 	modified:   spoke1-network.tf

--- aws-main.tf
+++ aws-main.tf
@@ -4,22 +4,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-   tags = {
-    Name = "main-vpc"
-  }
-}
-resource "aws_subnet" "main-subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.5.0/24"
-  depends_on = [aws_vpc.main]
-
-  tags = {
-    Name = "main-subnet"
-  }
-}
-output "main-vpc-output" {
-  value = aws_vpc.main.id
-}
-#
